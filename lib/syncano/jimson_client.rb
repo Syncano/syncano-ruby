44df0f89544f975ebac720136997e082dbf6ed1b
Piotr Zadrożny <piotr.zadrozny@webplus.net.pl>
2014-11-20 15:30:25 +0100

    Added user agent header to requests.

--- lib/syncano/jimson_client.rb
+++ lib/syncano/jimson_client.rb
@@ -2,6 +2,38 @@
 module Jimson
   # Overwritten helper from Jimson gem
   class ClientHelper
+    # Overwritten send_single_request method, so it now adds header with the user agent
+    # @return [Array] collection of responses
+    def send_single_request(method, args)
+      post_data = {
+          'jsonrpc' => JSON_RPC_VERSION,
+          'method'  => method,
+          'params'  => args,
+          'id'      => self.class.make_id
+      }.to_json
+      resp = RestClient.post(@url, post_data, :content_type => 'application/json', :user_agent => "syncano-ruby-#{Syncano::VERSION}")
+      if resp.nil? || resp.body.nil? || resp.body.empty?
+        raise Jimson::ClientError::InvalidResponse.new
+      end
+
+      return resp.body
+
+    rescue Exception, StandardError
+      raise Jimson::ClientError::InternalError.new($!)
+    end
+
+    # Overwritten send_batch_request method, so it now adds header with the user agent
+    # @return [Array] collection of responses
+    def send_batch_request(batch)
+      post_data = batch.to_json
+      resp = RestClient.post(@url, post_data, :content_type => 'application/json', :user_agent => "syncano-ruby-#{Syncano::VERSION}")
+      if resp.nil? || resp.body.nil? || resp.body.empty?
+        raise Jimson::ClientError::InvalidResponse.new
+      end
+
+      return resp.body
+    end
+
     # Overwritten send_batch method, so it now returns collection of responses
     # @return [Array] collection of responses
     def send_batch
