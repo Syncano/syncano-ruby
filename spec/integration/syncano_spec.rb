4f1c033ccad44d351e3e7b9c01b65d8ef127516f
Maciej Lotkowski <maciej.lotkowski@gmail.com>
2015-03-03 18:54:33 +0100

    Simplified test

--- spec/integration/syncano_spec.rb
+++ spec/integration/syncano_spec.rb
@@ -5,11 +5,9 @@ WebMock.allow_net_connect!
 describe Syncano do
   before(:all) do
     @api = Syncano.connect(api_key: ENV['INTEGRATION_TEST_API_KEY'])
-    @instance = begin
-      @api.instances.find 'Butchers'
-    rescue Syncano::ClientError
-      @api.instances.create(name: 'Butchers')
-    end
+    @api.instances.all.each &:destroy
+
+    @instance = @api.instances.create(name: 'butchers')
   end
 
   it 'should raise an error on not found instance' do
@@ -17,8 +15,6 @@ describe Syncano do
   end
 
   it 'work with classes and objects' do
-    @instance.classes.find('sausage').destroy rescue Syncano::ClientError
-
     klass = @instance.classes.create(name: 'sausage', schema: [{name: 'name', type: 'string' }])
 
     expect(klass.objects.all.count).to eq(0)
